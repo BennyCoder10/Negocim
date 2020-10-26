6c03aa20b4b5f4cad7398ec995c7559002e90b52
Benny-code <benpark1982@gmail.com>
2020-10-26 19:49:33 +0100

    mobile pages

--- js/index.js
+++ js/index.js
@@ -24,15 +24,15 @@ var i;
 
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var dropdownContent = this.nextElementSibling;
-  if (dropdownContent.style.display === "block") {
-  dropdownContent.style.display = "none";
-  } else {
-  dropdownContent.style.display = "block";
-  }
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
   });
-}
+};
 
 // Close the dropdown if the user clicks outside of it
 // window.onclick = function(event) {
@@ -118,4 +118,4 @@ if (currentStep == 0)
       prevBtn.setAttribute("onclick", "window.location='simulateur.html'");  
     }
   }
-} 
+};
--- js/lexique.js
+++ js/lexique.js
@@ -1,17 +1,20 @@
 function search() {
-  var input, filter, ul, li, a, i, txtValue;
+  var input, filter, contentsArea, content, a, i, txtValue;
   input = document.getElementById("search");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("lexique-contents");
-  li = ul.getElementsByClassName("content");
+  contentsArea = document.getElementById("lexique-contents");
+  content = contentsArea.getElementsByClassName("content");
 
-  for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("h3")[0];
+  for (i = 0; i < content.length; i++) {
+      a = content[i].getElementsByTagName("h3")[0];
       txtValue = a.textContent || a.innerText;
+      
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          li[i].style.display = "";
+        content[i].style.display = "";
       } else {
-          li[i].style.display = "none";
+        content[i].style.display = "none";
+        //   ul.textContent = "<h3>No Result</h3>";          
       }  
   };
 };
+
