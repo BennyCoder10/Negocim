a1a5cf5b1d07e4022d2a524f0c64f56558d37359
Benny-code <benpark1982@gmail.com>
2020-12-07 12:52:33 +0100

    working on carousel

--- js/terrain.js
+++ js/terrain.js
@@ -40,12 +40,101 @@ if (main.getAttribute("id") === "terrain_presentation") {
   };
 
 
-  // window.addEventListener('mouseup', function(event){
-  //   var modalImage = document.getElementById('modal-content');
-  //   if(event.target !== modalImage && event.target.parentNode !== modalImage) {
-  //     closeModal();
+
+  // CAROUSEL 
+  const carouselImages = document.getElementsByTagName("figure")[0].getElementsByTagName("img");
+
+  // if (carouselImages.length < 3) {
+  //   var i;
+  //   for (i = 0; i < carouselImages.length; i++) {
+  //     carouselImages[i].style.width = "40%"; 
+  //     carouselImages[i].style.position = "static"; 
   //   }
-  // });
+     
+  
+  // } else {
+
+  //   window.addEventListener('load', () => {
+  //     var
+  //       carousels = document.querySelectorAll('.carousel')
+  //     ;
+
+  //     for (var i = 0; i < carousels.length; i++) {
+  //       carousel(carousels[i]);
+  //     }
+  //   });
+
+  //   function carousel(root) {
+  //     var
+  //       figure = root.querySelector('figure'),
+  //       nav = root.querySelector('nav'),
+  //       images = figure.children,
+  //       n = images.length,
+  //       gap = root.dataset.gap || 0,
+  //       bfc = 'bfc' in root.dataset,
+        
+  //       theta =  2 * Math.PI / n,
+  //       currImage = 0
+  //     ;
+      
+  //     setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
+  //     window.addEventListener('resize', () => { 
+  //       setupCarousel(n, parseFloat(getComputedStyle(images[0]).width)) 
+  //     });
+
+  //     setupNavigation();
+
+  //     function setupCarousel(n, s) {
+  //       var	
+  //         apothem = s / (2 * Math.tan(Math.PI / n))
+  //       ;
+        
+  //       figure.style.transformOrigin = `50% 50% ${- apothem}px`;
+
+  //       for (var i = 0; i < n; i++)
+  //         images[i].style.padding = `${gap}px`;
+  //       for (i = 1; i < n; i++) {
+  //         images[i].style.transformOrigin = `50% 50% ${- apothem}px`;
+  //         images[i].style.transform = `rotateY(${i * theta}rad)`;
+  //       }
+  //       if (bfc)
+  //         for (i = 0; i < n; i++)
+  //           images[i].style.backfaceVisibility = 'hidden';
+        
+  //       rotateCarousel(currImage);
+  //     }
+
+  //     function setupNavigation() {
+  //       nav.addEventListener('click', onClick, true);
+        
+  //       function onClick(e) {
+  //         e.stopPropagation();
+          
+  //         var t = e.target;
+  //         if (t.tagName.toUpperCase() != 'BUTTON')
+  //           return;
+          
+  //         if (t.classList.contains('next')) {
+  //           currImage++;
+  //         }
+  //         else {
+  //           currImage--;
+  //         }
+          
+  //         rotateCarousel(currImage);
+  //       }        
+  //     }
+
+  //     function rotateCarousel(imageIndex) {
+  //       figure.style.transform = `rotateY(${imageIndex * -theta}rad)`;
+  //     }    
+  //   }
+  // } 
+  // END OF CAROUSEL
+
+  
+
+
 }
 
 
